e0a13aca405c67514f8a07b3a4f03fd7c31b68e9
Mario Avolio <64833154+MarioAvolio@users.noreply.github.com>
2021-03-10 22:27:21 +0100

    Adding EmbASP-7.4.0-py2.py3-none-any.whl

--- application/main.py
+++ application/main.py
@@ -1,4 +1,13 @@
+import os
+
+import pip
+
 from application.GameLoop import GameLoop
+from application.Settings_ import Settings
+
+
+def install_whl(path):
+    pip.main(['install', path])
 
 
 def main():
@@ -7,4 +16,5 @@ def main():
 
 
 if __name__ == '__main__':
+    install_whl(os.path.join(Settings.resource_path, "EmbASP-7.4.0-py2.py3-none-any.whl"))
     main()
