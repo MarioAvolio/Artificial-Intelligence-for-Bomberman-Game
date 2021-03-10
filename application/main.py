6fd3008b7751c98604fcbc5269fe05008e62cb3d
Mario Avolio <64833154+MarioAvolio@users.noreply.github.com>
2021-03-10 22:59:12 +0100

    adding DLV2

--- application/main.py
+++ application/main.py
@@ -1,6 +1,8 @@
 import os
 
 import pip
+from platforms.desktop.desktop_handler import DesktopHandler
+from specializations.dlv2.desktop.dlv2_desktop_service import DLV2DesktopService
 
 from application.GameLoop import GameLoop
 from application.Settings_ import Settings
@@ -13,8 +15,9 @@ def install_whl(path):
 def main():
     gl = GameLoop()
     gl.start()
+    handler = DesktopHandler(DLV2DesktopService(os.path.join(Settings.resource_path, "DLV2.exe")))
 
 
 if __name__ == '__main__':
-    install_whl(os.path.join(Settings.resource_path, "EmbASP-7.4.0-py2.py3-none-any.whl"))
+    # install_whl(os.path.join(Settings.resource_path, "EmbASP-7.4.0-py2.py3-none-any.whl")) # EMBASP INSTALLER
     main()
